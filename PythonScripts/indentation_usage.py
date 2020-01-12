7c1749b59823574afdaf03e76c5941f109187319
jeremy <jeremy@golangdev.jeremycoando.com>
2020-01-12 13:01:10 -0700

    Version1.1

---
+++ PythonScripts/indentation_usage.py
@@ -0,0 +1,5 @@
+print("This is about indentations")
+if 3 > 1:
+    print("using if condition")
+    print("we are comparing 3 and 1")
+print("We are ok with indentations")
---
+++ PythonScripts/special_char.py
@@ -0,0 +1,4 @@
+# print("Welcome to python scripting")
+# print("We are working with some special chars")
+print("Welcome to python scripting.\nWe are working with special chars")
+
