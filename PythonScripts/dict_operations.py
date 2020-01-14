f12d9fa6b3ce9e48eaa116fc413a5b5bc5e7725e
jeremy <jeremy@python.ultralearn.com>
2020-01-13 19:54:57 -0700

    Version1.4

---
+++ PythonScripts/dict_operations.py
@@ -0,0 +1,93 @@
+# my_dict={}
+# print(my_dict,type(my_dict))
+# print(bool(my_dict))
+
+my_dict={'fruit':'apple','animal':'fox',1:'one','two':2}
+# print(my_dict)
+# print(my_dict['fruit'])
+# print(my_dict.get('animal'))
+# print(my_dict.get['three'])
+# print(my_dict.get('three'))
+# print(my_dict)
+# my_dict.clear()
+# print(my_dict)
+
+# my_dict['three']=3
+# print(my_dict)
+# my_dict['three']=56
+# print(my_dict)
+# print(my_dict.keys())
+# print(my_dict.values())
+# print(my_dict.items())
+# y=my_dict.copy()
+# print(y)
+
+# my_new_dict={'four':'4'}
+# my_dict.update(my_new_dict)
+# print(my_dict)
+
+# my_dict.pop('four')
+# print(my_dict)
+# removed_item=my_dict.popitem()
+# print(my_dict)
+# print(removed_item)
+
+
+# keys={'a','e','i','o','u'}
+
+# new_dict=dict.fromkeys(keys)
+
+# print(new_dict)
+# new_dict['a']="first alphabet"
+# print(new_dict)
+
+my_dict={}
+my_dict.setdefault('k',45)
+print(my_dict)
+my_dict={'fruit':'apple'}
+my_dict.setdefault('fruit','orange')
+print(my_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- PythonScripts/working_with_tuple.py
+++ PythonScripts/working_with_tuple.py
@@ -5,12 +5,20 @@ my_tuple=(3,4,5)
 # print(bool(my_empty))
 # print(bool(my_tuple))
 
-my_tuple=(3,4,[5,6,7],8,9)
-print(my_tuple)
-print(my_tuple[2][1])
+# my_tuple=(3,4,[5,6,7],8,9)
+# print(my_tuple)
+# print(my_tuple[2][1])
 
 # my_tuple[0]=34
 
+# my_tuple=(3,4,5,6,78,2,65,0)
+# print(my_tuple[3:])
+# print(my_tuple[:6])
+
+# print(my_tuple[3:5])
+# y=5,
+x=5,8,9
+print(x,type(x))
 
 
 
