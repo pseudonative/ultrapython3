c0a0471011c80f32b8ab5bbd7bc1b32b4045fda0
jeremy <jeremy@python.ultralearn.com>
2020-01-25 12:05:48 -0700

    Version1.1.5

---
+++ PythonScripts/function_with_simple_argumets.py
@@ -0,0 +1,32 @@
+
+# def get_result(value):
+#     result=value+10
+#     print(f'Your result is: {result}')
+#     return None 
+
+# def main():
+#     #global num
+#     num=eval(input("Enter your number: "))
+#     get_result(num)
+#     return None
+
+# main()
+
+
+
+def get_add(a,b):
+    aresult=a+b 
+    print(f'The addition of {a} and {b} is: {aresult}')
+    return None
+def get_sub(a,b):
+    sresult=a-b 
+    print(f'The subtraction of {a} and {b} is: {sresult}')
+    return None
+def main():
+    a=eval(input("Enter your first number: "))
+    b=eval(input("Enter your second number:"))
+    get_add(a,b)
+    get_sub(a,b)
+    return None
+main()
+
