d2112b8c0e93d4f735b5f3222b97b14dc93ad536
Your Name <jaycoan@yahoo.com>
2020-01-27 07:22:01 -0700

    Version1.1.10 '

---
+++ pythonBootCamp/mileage.py
@@ -0,0 +1,11 @@
+print("How many kilometers did you cycle today?")
+kms=input()
+miles=float(kms)/1.60934
+miles=round(miles,2)
+print(f"Your {kms}km rid is  {miles} miles")
+
+# route(thing to round, how many decimal points)
+
+
+
+
---
+++ pythonBootCamp/string_concat.py
@@ -0,0 +1,29 @@
+# str_one="your"
+# str_two="face"
+# str_three=str_one + " " + str_two
+# print(str_three)
+
+# username="bluethecat"
+# print("Hello ther and welcome to the game, "+username)
+
+# guess=8
+# print(f"Your guess of {guess} was incorrect!")
+# print(f"Your guess of {guess+1} was incorrect!")
+
+
+# first="Venus"
+# last="Williams"
+# formatted="First Name: {},Last Name:{}".format(first,last)
+# print(formatted)
+
+# first="Venus"
+# last="Williams"
+# formatted=f"First Name: {first},Last Name:{last}"
+# print(formatted)
+
+
+
+
+
+
+
