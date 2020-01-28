bbed50bdc98844453a3ceca0408544fb77b08079
Jeremy Coando <jeremy.coando@beatport.com>
2020-01-28 11:27:41 -0700

    Version1.1.12

--- PythonScripts/git_req_ext_files.py
+++ PythonScripts/git_req_ext_files.py
@@ -1,22 +1,5 @@
 #!/usr/local/bin/python3.7
-import os
-req_path=input("Enter your directory path: ")
-if os.path.isfile(req_path):
-    print(f"The Given path {req_path} is a file. Please pass in a directory ")
-else:
-    all_f_ds=os.listdir(req_path)
-    if len(all_f_ds)==0:
-        print(f"The Given Path is {req_path} an empty path")
-    else:
-        req_ex=input("Enter the required files extension .py/ .sh/ .log .txt: ")
-        req_files=[]
-        for each_f in all_f_ds:
-            if each_f.endswith(req_ex):
-                req_files.append(each_f)
-        if len(req_files)==0:
-            print(f"There are no {req_ex} files in the location of {req_path}")
-        else:
-            print(f"There are {len(req_files)} files in the location of {req_path} with an extension of {req_ex} ")
-            print(f"So the files are: {req_files}")
+
+
 
 
