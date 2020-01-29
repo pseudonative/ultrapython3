176b2df05948bfba0cac3c3639a36ee3e683edd1
Your Name <jaycoan@yahoo.com>
2020-01-28 21:29:24 -0700

    Version1.1.13

---
+++ pythonBootCamp/while.py
@@ -0,0 +1,20 @@
+# msg=input("Whats the secret password? ")
+# while msg!="bananas":
+#     print("WRONG")
+#     msg=input("whats the secret password? ")
+# print("Correct!")
+
+
+# for num in range(1,11):
+#     print(num)
+
+num=1
+while num<11:
+    print(num)
+    num+=2
+
+
+
+
+
+
