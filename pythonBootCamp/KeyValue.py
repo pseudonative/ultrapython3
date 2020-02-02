aaad859719d22b0add46378e883df950fad2c76a
jeremycoandoubuntu <jeremycoandoubuntu@example.com>
2020-02-01 21:02:31 -0700

    first python commit from Ubuntu

---
+++ pythonBootCamp/KeyValue.py
@@ -0,0 +1,15 @@
+
+
+instructor={
+    "name":"Colt",
+    "owns_dog":True,
+    "num_courses":4,
+    "favorite_language":"Python",
+    "is hilarious":False,
+    44:"my favorite number!"
+}
+
+for key,value in instructor.items():
+    print(key,value)
+
+
