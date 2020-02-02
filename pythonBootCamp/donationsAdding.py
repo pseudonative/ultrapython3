bbadf8520b1b7d424d89b608b37c533bdfe4ade5
jeremycoandoubuntu <jeremycoandoubuntu@example.com>
2020-02-01 22:04:43 -0700

    From dev branch

---
+++ pythonBootCamp/donationsAdding.py
@@ -0,0 +1,18 @@
+# also practicing git
+
+donations=dict(sam=25.0,lena=88.99,chuck=13.0,linus=99.5,stan=150.0,lisa=50.25,harrison=10.0)
+
+# total_donations=0
+# for donation in donations.values():
+#     total_donations+=donation 
+#     print(total_donations)
+
+# total_donations=sum(donation for donation in donations.values())
+# print(total_donations)
+
+
+total_donations=sum(donations.values())
+print(total_donations)
+
+
+
