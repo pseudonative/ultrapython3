709e0226135360f92d8a024de1b8aaa4d7e3c537
Your Name <jaycoan@yahoo.com>
2020-02-01 22:16:18 -0700

    Found a bug from Dev, changed ready for production

--- pythonBootCamp/helloworld1.py
+++ pythonBootCamp/helloworld1.py
@@ -1,5 +1,6 @@
+# found a typo corrected ready for production
 h ="hello"
 w ="world"
-h_w=(h+' '+2)
+h_w=(h+' '+w)
 print(h_w)
 
--- pythonBootCamp/helloworld2.py
+++ pythonBootCamp/helloworld2.py
@@ -1,1 +1,5 @@
-print("hello world")
+# print("hello world")
+# here's a better syntax code corrected
+
+p="hello world"
+print(p)
