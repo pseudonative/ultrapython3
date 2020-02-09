b038be7ac814bedc2f2b9fb8bf870b47e971e41d
jeremy <jeremy@thinkpad.example.com>
2020-02-09 05:58:17 -0700

    from laptop:

---
+++ pythonBootCamp/sets.py
@@ -0,0 +1,9 @@
+math_students={"Matthew","Helen","Prashant","James","Aparna"}
+biology_students={"Jane","Matthew","Charlotte","Musut","Oliver","James"}
+
+# To generate a set with all my unique students
+math_students | biology_students 
+
+# To generate a set with students who are in both courses
+
+math_students & biology_students
